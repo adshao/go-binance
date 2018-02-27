adshao/go-binance

commit f4b1b3bf5cc2e2d98d20c653f1b0ca9bc24d002f

code quality improvements (#21)
* avoid naked returns

* add missing error checks

ticker_service.go
@@ -18,14 +18,14 @@ func (s *ListBookTickersService) Do(ctx context.Context, opts ...RequestOption) 
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return []*BookTicker{}, nil
 	}
 	res = make([]*BookTicker, 0)
 	err = json.Unmarshal(data, &res)
 	if err != nil {
-		return
+		return []*BookTicker{}, nil
 	}
-	return
+	return res, nil
 }
 
 // BookTickerService list symbol's book ticker
@@ -49,14 +49,14 @@ func (s *BookTickerService) Do(ctx context.Context, opts ...RequestOption) (res 
 	r.setParam("symbol", s.symbol)
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return nil, err
 	}
 	res = new(BookTicker)
 	err = json.Unmarshal(data, &res)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return
+	return res, nil
 }
 
 // BookTicker define book ticker info
@@ -81,14 +81,14 @@ func (s *ListPricesService) Do(ctx context.Context, opts ...RequestOption) (res 
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return []*SymbolPrice{}, nil
 	}
 	res = make([]*SymbolPrice, 0)
 	err = json.Unmarshal(data, &res)
 	if err != nil {
-		return
+		return []*SymbolPrice{}, nil
 	}
-	return
+	return res, nil
 }
 
 // SymbolPrice define symbol and price pair
@@ -118,14 +118,14 @@ func (s *PriceChangeStatsService) Do(ctx context.Context, opts ...RequestOption)
 	r.setParam("symbol", s.symbol)
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return res, nil
 	}
 	res = new(PriceChangeStats)
 	err = json.Unmarshal(data, res)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return
+	return res, nil
 }
 
 // PriceChangeStats define price change stats

account_service.go
@@ -19,14 +19,14 @@ func (s *GetAccountService) Do(ctx context.Context, opts ...RequestOption) (res 
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return nil, err
 	}
 	res = new(Account)
 	err = json.Unmarshal(data, res)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return
+	return res, nil
 }
 
 // Account define account info

client.go
@@ -50,9 +50,9 @@ func currentTimestamp() int64 {
 func newJSON(data []byte) (j *simplejson.Json, err error) {
 	j, err = simplejson.NewJson(data)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return
+	return j, nil
 }
 
 // NewClient initialize an API client instance with API key and secret key.
@@ -96,7 +96,7 @@ func (c *Client) parseRequest(r *request, opts ...RequestOption) (err error) {
 	}
 	err = r.validate()
 	if err != nil {
-		return
+		return err
 	}
 
 	fullURL := fmt.Sprintf("%s%s", c.BaseURL, r.endpoint)
@@ -121,7 +121,10 @@ func (c *Client) parseRequest(r *request, opts ...RequestOption) (err error) {
 	if r.secType == secTypeSigned {
 		raw := fmt.Sprintf("%s%s", queryString, bodyString)
 		mac := hmac.New(sha256.New, []byte(c.SecretKey))
-		mac.Write([]byte(raw))
+		_, err = mac.Write([]byte(raw))
+		if err != nil {
+			return err
+		}
 		v := url.Values{}
 		v.Set(signatureKey, fmt.Sprintf("%x", (mac.Sum(nil))))
 		if queryString == "" {
@@ -138,17 +141,17 @@ func (c *Client) parseRequest(r *request, opts ...RequestOption) (err error) {
 	r.fullURL = fullURL
 	r.header = header
 	r.body = body
-	return
+	return nil
 }
 
 func (c *Client) callAPI(ctx context.Context, r *request, opts ...RequestOption) (data []byte, err error) {
 	err = c.parseRequest(r, opts...)
 	if err != nil {
-		return
+		return []byte{}, err
 	}
 	req, err := http.NewRequest(r.method, r.fullURL, r.body)
 	if err != nil {
-		return
+		return []byte{}, err
 	}
 	req = req.WithContext(ctx)
 	req.Header = r.header
@@ -159,13 +162,20 @@ func (c *Client) callAPI(ctx context.Context, r *request, opts ...RequestOption)
 	}
 	res, err := f(req)
 	if err != nil {
-		return
+		return []byte{}, err
 	}
 	data, err = ioutil.ReadAll(res.Body)
 	if err != nil {
-		return
+		return []byte{}, err
 	}
-	defer res.Body.Close()
+	defer func() {
+		cerr := res.Body.Close()
+		// Only overwrite the retured error if the original error was nil and an
+		// error occurred while closing the body.
+		if err == nil && cerr != nil {
+			err = cerr
+		}
+	}()
 	c.debug("response: %#v", res)
 	c.debug("response body: %s", string(data))
 
@@ -177,7 +187,7 @@ func (c *Client) callAPI(ctx context.Context, r *request, opts ...RequestOption)
 		}
 		return nil, apiErr
 	}
-	return
+	return data, nil
 }
 
 // NewPingService init ping service

depth_service.go
@@ -35,11 +35,11 @@ func (s *DepthService) Do(ctx context.Context, opts ...RequestOption) (res *Dept
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return nil, err
 	}
 	j, err := newJSON(data)
 	if err != nil {
-		return
+		return nil, err
 	}
 	res = new(DepthResponse)
 	res.LastUpdateID = j.Get("lastUpdateId").MustInt64()
@@ -61,7 +61,7 @@ func (s *DepthService) Do(ctx context.Context, opts ...RequestOption) (res *Dept
 			Quantity: item.GetIndex(1).MustString(),
 		}
 	}
-	return
+	return res, nil
 }
 
 // DepthResponse define depth info with bids and asks

exchange_info_service.go
@@ -19,15 +19,15 @@ func (s *ExchangeInfoService) Do(ctx context.Context, opts ...RequestOption) (re
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return nil, err
 	}
 	res = new(ExchangeInfo)
 	err = json.Unmarshal(data, res)
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	return
+	return res, nil
 }
 
 // ExchangeInfo exchange info

kline_service.go
@@ -64,11 +64,11 @@ func (s *KlinesService) Do(ctx context.Context, opts ...RequestOption) (res []*K
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return []*Kline{}, err
 	}
 	j, err := newJSON(data)
 	if err != nil {
-		return
+		return []*Kline{}, err
 	}
 	num := len(j.MustArray())
 	res = make([]*Kline, num)
@@ -76,7 +76,7 @@ func (s *KlinesService) Do(ctx context.Context, opts ...RequestOption) (res []*K
 		item := j.GetIndex(i)
 		if len(item.MustArray()) < 11 {
 			err = fmt.Errorf("invalid kline response")
-			return
+			return []*Kline{}, err
 		}
 		res[i] = &Kline{
 			OpenTime:                 item.GetIndex(0).MustInt64(),
@@ -92,7 +92,7 @@ func (s *KlinesService) Do(ctx context.Context, opts ...RequestOption) (res []*K
 			TakerBuyQuoteAssetVolume: item.GetIndex(10).MustString(),
 		}
 	}
-	return
+	return res, nil
 }
 
 // Kline define kline info

order_service.go
@@ -103,23 +103,23 @@ func (s *CreateOrderService) createOrder(ctx context.Context, endpoint string, o
 	r.setFormParams(m)
 	data, err = s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return []byte{}, err
 	}
-	return
+	return data, nil
 }
 
 // Do send request
 func (s *CreateOrderService) Do(ctx context.Context, opts ...RequestOption) (res *CreateOrderResponse, err error) {
 	data, err := s.createOrder(ctx, "/api/v3/order", opts...)
 	if err != nil {
-		return
+		return nil, err
 	}
 	res = new(CreateOrderResponse)
 	err = json.Unmarshal(data, res)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return
+	return res, nil
 }
 
 // Test send test api to check if the request is valid
@@ -165,14 +165,14 @@ func (s *ListOpenOrdersService) Do(ctx context.Context, opts ...RequestOption) (
 	r.setParam("symbol", s.symbol)
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return []*Order{}, err
 	}
 	res = make([]*Order, 0)
 	err = json.Unmarshal(data, &res)
 	if err != nil {
-		return
+		return []*Order{}, err
 	}
-	return
+	return res, nil
 }
 
 // GetOrderService get an order
@@ -217,14 +217,14 @@ func (s *GetOrderService) Do(ctx context.Context, opts ...RequestOption) (res *O
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return nil, err
 	}
 	res = new(Order)
 	err = json.Unmarshal(data, res)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return
+	return res, nil
 }
 
 // Order define order info
@@ -286,14 +286,14 @@ func (s *ListOrdersService) Do(ctx context.Context, opts ...RequestOption) (res 
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return []*Order{}, err
 	}
 	res = make([]*Order, 0)
 	err = json.Unmarshal(data, &res)
 	if err != nil {
-		return
+		return []*Order{}, err
 	}
-	return
+	return res, nil
 }
 
 // CancelOrderService cancel an order
@@ -348,14 +348,14 @@ func (s *CancelOrderService) Do(ctx context.Context, opts ...RequestOption) (res
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return nil, err
 	}
 	res = new(CancelOrderResponse)
 	err = json.Unmarshal(data, res)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return
+	return res, nil
 }
 
 // CancelOrderResponse define response of canceling order

server_service.go
@@ -16,10 +16,7 @@ func (s *PingService) Do(ctx context.Context, opts ...RequestOption) (err error)
 		endpoint: "/api/v1/ping",
 	}
 	_, err = s.c.callAPI(ctx, r, opts...)
-	if err != nil {
-		return
-	}
-	return
+	return err
 }
 
 // ServerTimeService get server time
@@ -35,12 +32,12 @@ func (s *ServerTimeService) Do(ctx context.Context, opts ...RequestOption) (serv
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return
+		return 0, err
 	}
 	j, err := newJSON(data)
 	if err != nil {
-		return
+		return 0, err
 	}
 	serverTime = j.Get("serverTime").MustInt64()
-	return
+	return serverTime, nil
 }

commit 19938cae92bf90dd53cbae1b611a2d5a5376a795

fix regression introduced in f4b1b3bf5cc2e2

ticker_service.go
@@ -18,12 +18,12 @@ func (s *ListBookTickersService) Do(ctx context.Context, opts ...RequestOption) 
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return []*BookTicker{}, nil
+		return []*BookTicker{}, err
 	}
 	res = make([]*BookTicker, 0)
 	err = json.Unmarshal(data, &res)
 	if err != nil {
-		return []*BookTicker{}, nil
+		return []*BookTicker{}, err
 	}
 	return res, nil
 }
@@ -81,12 +81,12 @@ func (s *ListPricesService) Do(ctx context.Context, opts ...RequestOption) (res 
 	}
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return []*SymbolPrice{}, nil
+		return []*SymbolPrice{}, err
 	}
 	res = make([]*SymbolPrice, 0)
 	err = json.Unmarshal(data, &res)
 	if err != nil {
-		return []*SymbolPrice{}, nil
+		return []*SymbolPrice{}, err
 	}
 	return res, nil
 }
@@ -118,7 +118,7 @@ func (s *PriceChangeStatsService) Do(ctx context.Context, opts ...RequestOption)
 	r.setParam("symbol", s.symbol)
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
-		return res, nil
+		return res, err
 	}
 	res = new(PriceChangeStats)
 	err = json.Unmarshal(data, res)