3cb6ce9217ca2354113080616adf89dd10896e65
Caleb Sutton <caleb.j.sutton@gmail.com>
2018-07-24 17:22:42 -0400

    Checking something

--- client.go
+++ client.go
@@ -69,7 +69,7 @@ func NewClient(apiKey, secretKey string) *Client {
 	return &Client{
 		APIKey:     apiKey,
 		SecretKey:  secretKey,
-		BaseURL:    "https://www.api.binance.com",
+		BaseURL:    "https://www.binance.com",
 		UserAgent:  "Binance/golang",
 		HTTPClient: http.DefaultClient,
 		Logger:     log.New(os.Stderr, "Binance-golang ", log.LstdFlags),
