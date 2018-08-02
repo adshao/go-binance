3f763f3a75b18f66f9e40e05affd81482cc6bf9b
Caleb Sutton <caleb.j.sutton@gmail.com>
2018-08-02 09:19:10 -0400

    Added test case for number value in a Symbol filter

--- exchange_info_service_test.go
+++ exchange_info_service_test.go
@@ -26,7 +26,7 @@ func (s *exchangeInfoServiceTestSuite) TestExchangeInfo() {
 				"quotePrecision": 8,
 				"orderTypes":["LIMIT","LIMIT_MAKER","MARKET","STOP_LOSS_LIMIT","TAKE_PROFIT_LIMIT"],
 				"icebergAllowed": true,
-				"filters":[{"filterType":"PRICE_FILTER","minPrice":"0.00000100","maxPrice":"100000.00000000","tickSize":"0.00000100"},{"filterType":"LOT_SIZE","minQty":"0.00100000","maxQty":"100000.00000000","stepSize":"0.00100000"},{"filterType":"MIN_NOTIONAL","minNotional":"0.00100000"}]
+				"filters":[{"filterType":"PRICE_FILTER","minPrice":"0.00000100","maxPrice":"100000.00000000","tickSize":"0.00000100"},{"filterType":"LOT_SIZE","minQty":"0.00100000","maxQty":"100000.00000000","stepSize":"0.00100000"},{"filterType":"MIN_NOTIONAL","minNotional":"0.00100000"},{"filterType": "MAX_NUM_ALGO_ORDERS", "maxNumAlgoOrders": 5}]
 			}
 		]
 	}`)
@@ -53,6 +53,7 @@ func (s *exchangeInfoServiceTestSuite) TestExchangeInfo() {
 					{"filterType": "PRICE_FILTER", "minPrice": "0.00000100", "maxPrice": "100000.00000000", "tickSize": "0.00000100"},
 					{"filterType": "LOT_SIZE", "minQty": "0.00100000", "maxQty": "100000.00000000", "stepSize": "0.00100000"},
 					{"filterType": "MIN_NOTIONAL", "minNotional": "0.00100000"},
+					{"filterType": "MAX_NUM_ALGO_ORDERS", "maxNumAlgoOrders": 5},
 				},
 			},
 		},
