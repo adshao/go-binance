37ce96c6e8571e6afd72ba958f8a34d8972d999f
Caleb <cjamessutton@users.noreply.github.com>
2018-08-02 09:33:15 -0400

    Fixed the ExchangeInfo Symbol struct to allow for generic filter value types (#59)
    * Fixed the base API url
    
    * Checking something
    
    * Changed Symbol struct field Filters to contain interfaces
    
    * Added test case for number value in a Symbol filter

--- exchange_info_service_test.go
+++ exchange_info_service_test.go
@@ -26,7 +26,7 @@ func (s *exchangeInfoServiceTestSuite) TestExchangeInfo() {
 				"quotePrecision": 8,
 				"orderTypes":["LIMIT","LIMIT_MAKER","MARKET","STOP_LOSS_LIMIT","TAKE_PROFIT_LIMIT"],
 				"icebergAllowed": true,
-				"filters":[{"filterType":"PRICE_FILTER","minPrice":"0.00000100","maxPrice":"100000.00000000","tickSize":"0.00000100"},{"filterType":"LOT_SIZE","minQty":"0.00100000","maxQty":"100000.00000000","stepSize":"0.00100000"},{"filterType":"MIN_NOTIONAL","minNotional":"0.00100000"}]
+				"filters":[{"filterType":"PRICE_FILTER","minPrice":"0.00000100","maxPrice":"100000.00000000","tickSize":"0.00000100"},{"filterType":"LOT_SIZE","minQty":"0.00100000","maxQty":"100000.00000000","stepSize":"0.00100000"},{"filterType":"MIN_NOTIONAL","minNotional":"0.00100000"},{"filterType": "MAX_NUM_ALGO_ORDERS", "maxNumAlgoOrders": 5}]
 			}
 		]
 	}`)
@@ -49,10 +49,11 @@ func (s *exchangeInfoServiceTestSuite) TestExchangeInfo() {
 				QuotePrecision:     8,
 				OrderTypes:         []string{"LIMIT", "LIMIT_MAKER", "MARKET", "STOP_LOSS_LIMIT", "TAKE_PROFIT_LIMIT"},
 				IcebergAllowed:     true,
-				Filters: []map[string]string{
+				Filters: []map[string]interface{}{
 					{"filterType": "PRICE_FILTER", "minPrice": "0.00000100", "maxPrice": "100000.00000000", "tickSize": "0.00000100"},
 					{"filterType": "LOT_SIZE", "minQty": "0.00100000", "maxQty": "100000.00000000", "stepSize": "0.00100000"},
 					{"filterType": "MIN_NOTIONAL", "minNotional": "0.00100000"},
+					{"filterType": "MAX_NUM_ALGO_ORDERS", "maxNumAlgoOrders": 5},
 				},
 			},
 		},
