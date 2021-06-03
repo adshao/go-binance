3692727a03f26697669ee3d79807ebcd1c6b2ca4
alphagot <zqs7399@163.com>
2021-06-03 11:40:16 +0800

    add multiple symbols depth for futures websocket

--- v2/futures/websocket_service.go
+++ v2/futures/websocket_service.go
@@ -508,6 +508,52 @@ func WsDiffDepthServe(symbol string, handler WsDepthHandler, errHandler ErrHandl
 	return wsDepthServe(symbol, "", nil, handler, errHandler)
 }
 
+// WsCombinedDepthServe is similar to WsPartialDepthServe, but it for multiple symbols
+func WsCombinedDepthServe(symbolLevels map[string]string, handler WsDepthHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := getCombinedEndpoint()
+	for s, l := range symbolLevels {
+		endpoint += fmt.Sprintf("%s@depth%s", strings.ToLower(s), l) + "/"
+	}
+	endpoint = endpoint[:len(endpoint)-1]
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		j, err := newJSON(message)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		event := new(WsDepthEvent)
+		data := j.Get("data").MustMap()
+		event.Event = data["e"].(string)
+		event.Time, _ = data["E"].(json.Number).Int64()
+		event.TransactionTime, _ = data["T"].(json.Number).Int64()
+		event.Symbol = data["s"].(string)
+		event.FirstUpdateID, _ = data["U"].(json.Number).Int64()
+		event.LastUpdateID, _ = data["u"].(json.Number).Int64()
+		event.PrevLastUpdateID, _ = data["pu"].(json.Number).Int64()
+		bidsLen := len(data["b"].([]interface{}))
+		event.Bids = make([]Bid, bidsLen)
+		for i := 0; i < bidsLen; i++ {
+			item := data["b"].([]interface{})[i].([]interface{})
+			event.Bids[i] = Bid{
+				Price:    item[0].(string),
+				Quantity: item[1].(string),
+			}
+		}
+		asksLen := len(data["a"].([]interface{}))
+		event.Asks = make([]Ask, asksLen)
+		for i := 0; i < asksLen; i++ {
+			item := data["a"].([]interface{})[i].([]interface{})
+			event.Asks[i] = Ask{
+				Price:    item[0].(string),
+				Quantity: item[1].(string),
+			}
+		}
+		handler(event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
 // WsDiffDepthServeWithRate serve websocket diff. depth handler with rate.
 func WsDiffDepthServeWithRate(symbol string, rate time.Duration, handler WsDepthHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
 	return wsDepthServe(symbol, "", &rate, handler, errHandler)
