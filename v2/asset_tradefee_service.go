27eef4a75c10785f867e93ecfdf24fd18f4b6f80
Your Name <you@example.com>
2021-11-22 16:19:21 +0800

    1

--- v2/asset_tradefee_service.go
+++ v2/asset_tradefee_service.go
@@ -5,30 +5,26 @@ import (
 	"encoding/json"
 )
 
-
 //查询手续费服务
 type AssetTradeFeeService struct {
 	c *Client
 
-	symbol string
+	symbol     string
 	recvWindow int64
 }
 
-
-func (s *AssetTradeFeeService) Symbol(a string) *AssetTradeFeeService{
+func (s *AssetTradeFeeService) Symbol(a string) *AssetTradeFeeService {
 	s.symbol = a
 	return s
 }
 
-func (s *AssetTradeFeeService) RecvWindow(a int64) *AssetTradeFeeService{
+func (s *AssetTradeFeeService) RecvWindow(a int64) *AssetTradeFeeService {
 	s.recvWindow = a
 	return s
 }
 
-
-
 // Do send request
-func (s *AssetTradeFeeService) Do(ctx context.Context, opts ...RequestOption) (res []*AssetTradeFeeResponse, err error) {
+func (s *AssetTradeFeeService) Do(ctx context.Context, opts ...RequestOption) (res []AssetTradeFeeResponse, err error) {
 	r := &request{
 		method:   "GET",
 		endpoint: "/sapi/v1/asset/tradeFee",
@@ -42,23 +38,20 @@ func (s *AssetTradeFeeService) Do(ctx context.Context, opts ...RequestOption) (r
 		r.setParam("recvWindow", s.recvWindow)
 	}
 
-
 	data, err := s.c.callAPI(ctx, r, opts...)
 	if err != nil {
 		return nil, err
 	}
-	res = make([]*AssetTradeFeeResponse, 0)
-	err = json.Unmarshal(data, res)
+	res = make([]AssetTradeFeeResponse, 0)
+	err = json.Unmarshal(data, &res)
 	if err != nil {
 		return nil, err
 	}
 	return res, nil
 }
 
-
 type AssetTradeFeeResponse struct {
-	Symbol  string  	`json:"symbol"`
-	MakerCommission  string `json:"maker_commission"`
-	TakerCommission  string `json:"taker_commission"`
+	Symbol          string `json:"symbol"`
+	MakerCommission string `json:"makerCommission"`
+	TakerCommission string `json:"takerCommission"`
 }
-
