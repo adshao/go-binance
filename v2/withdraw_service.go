b9a21fe4d0856b2b45cf47477be0cadd0daa3e6f
mm2175 <30251661+mm2175@users.noreply.github.com>
2022-01-08 11:54:55 +0800

    ListWithdrawsService add withdrawOrderId support (#334)
    * ListWithdrawsService add withdrawOrderId support
    
    * use string intead of float64 for number

--- v2/withdraw_service.go
+++ v2/withdraw_service.go
@@ -117,18 +117,25 @@ type CreateWithdrawResponse struct {
 //
 // See https://binance-docs.github.io/apidocs/spot/en/#withdraw-history-supporting-network-user_data
 type ListWithdrawsService struct {
-	c         *Client
-	coin      *string
-	status    *int
-	startTime *int64
-	endTime   *int64
-	offset    *int
-	limit     *int
+	c               *Client
+	coin            *string
+	withdrawOrderId *string
+	status          *int
+	startTime       *int64
+	endTime         *int64
+	offset          *int
+	limit           *int
 }
 
 // Coin sets the coin parameter.
 func (s *ListWithdrawsService) Coin(coin string) *ListWithdrawsService {
 	s.coin = &coin
+	return s
+}
+
+// WithdrawOrderId sets the withdrawOrderId parameter.
+func (s *ListWithdrawsService) WithdrawOrderId(withdrawOrderId string) *ListWithdrawsService {
+	s.withdrawOrderId = &withdrawOrderId
 	return s
 }
 
@@ -174,6 +181,9 @@ func (s *ListWithdrawsService) Do(ctx context.Context) (res []*Withdraw, err err
 	if s.coin != nil {
 		r.setParam("coin", *s.coin)
 	}
+	if s.withdrawOrderId != nil {
+		r.setParam("withdrawOrderId", *s.withdrawOrderId)
+	}
 	if s.status != nil {
 		r.setParam("status", *s.status)
 	}
@@ -208,10 +218,11 @@ type Withdraw struct {
 	ApplyTime       string `json:"applyTime"`
 	Coin            string `json:"coin"`
 	ID              string `json:"id"`
-	WithdrawOrderID string `json:"withdrawOrderID"`
+	WithdrawOrderID string `json:"withdrawOrderId"`
 	Network         string `json:"network"`
 	TransferType    int    `json:"transferType"`
 	Status          int    `json:"status"`
 	TransactionFee  string `json:"transactionFee"`
+	Info            string `json:"info"`
 	TxID            string `json:"txId"`
 }
