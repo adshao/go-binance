a3cd38daa46bd8fc663d13625fb4406e25703611
Pascal Casteran <15686972+pcasteran@users.noreply.github.com>
2022-02-11 22:50:18 +0100

    Added missing fields in Exchange Info response

--- v2/exchange_info_service.go
+++ v2/exchange_info_service.go
@@ -77,20 +77,23 @@ type RateLimit struct {
 
 // Symbol market symbol
 type Symbol struct {
-	Symbol                 string                   `json:"symbol"`
-	Status                 string                   `json:"status"`
-	BaseAsset              string                   `json:"baseAsset"`
-	BaseAssetPrecision     int                      `json:"baseAssetPrecision"`
-	QuoteAsset             string                   `json:"quoteAsset"`
-	QuotePrecision         int                      `json:"quotePrecision"`
-	QuoteAssetPrecision    int                      `json:"quoteAssetPrecision"`
-	OrderTypes             []string                 `json:"orderTypes"`
-	IcebergAllowed         bool                     `json:"icebergAllowed"`
-	OcoAllowed             bool                     `json:"ocoAllowed"`
-	IsSpotTradingAllowed   bool                     `json:"isSpotTradingAllowed"`
-	IsMarginTradingAllowed bool                     `json:"isMarginTradingAllowed"`
-	Filters                []map[string]interface{} `json:"filters"`
-	Permissions            []string                 `json:"permissions"`
+	Symbol                     string                   `json:"symbol"`
+	Status                     string                   `json:"status"`
+	BaseAsset                  string                   `json:"baseAsset"`
+	BaseAssetPrecision         int                      `json:"baseAssetPrecision"`
+	QuoteAsset                 string                   `json:"quoteAsset"`
+	QuotePrecision             int                      `json:"quotePrecision"`
+	QuoteAssetPrecision        int                      `json:"quoteAssetPrecision"`
+	BaseCommissionPrecision    int32                    `json:"baseCommissionPrecision"`
+	QuoteCommissionPrecision   int32                    `json:"quoteCommissionPrecision"`
+	OrderTypes                 []string                 `json:"orderTypes"`
+	IcebergAllowed             bool                     `json:"icebergAllowed"`
+	OcoAllowed                 bool                     `json:"ocoAllowed"`
+	QuoteOrderQtyMarketAllowed bool                     `json:"quoteOrderQtyMarketAllowed"`
+	IsSpotTradingAllowed       bool                     `json:"isSpotTradingAllowed"`
+	IsMarginTradingAllowed     bool                     `json:"isMarginTradingAllowed"`
+	Filters                    []map[string]interface{} `json:"filters"`
+	Permissions                []string                 `json:"permissions"`
 }
 
 // LotSizeFilter define lot size filter of symbol
