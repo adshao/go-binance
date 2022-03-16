9db19b9658fd57e90b0186213ba72c2fb033a85f
Marquitos <sonirico@users.noreply.github.com>
2022-03-16 19:28:37 +0100

    feat: add trade ID field to order fills

--- v2/order_service.go
+++ v2/order_service.go
@@ -176,6 +176,7 @@ type CreateOrderResponse struct {
 
 // Fill may be returned in an array of fills in a CreateOrderResponse.
 type Fill struct {
+	TradeID         int    `json:"tradeId"`
 	Price           string `json:"price"`
 	Quantity        string `json:"qty"`
 	Commission      string `json:"commission"`
