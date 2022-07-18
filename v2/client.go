434e9ad77922348f0ac907f6bc580dab075e3c9b
Amir Jani <amir.jani500@gmail.com>
2022-07-18 15:14:50 +0430

    feature: futures commission rate added + tests

--- v2/client.go
+++ v2/client.go
@@ -892,8 +892,3 @@ func (c *Client) NewRemoveLiquidityService() *RemoveLiquidityService {
 func (c *Client) NewQueryClaimedRewardHistoryService() *QueryClaimedRewardHistoryService {
 	return &QueryClaimedRewardHistoryService{c: c}
 }
-
-// NewCreateOrderService init creating order service
-func (c *Client) NewCommissionRateService() *CommissionRateService {
-	return &CommissionRateService{c: c}
-}
