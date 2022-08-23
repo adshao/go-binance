f3bd66ee84caca1dd0e69e517d34319e145790a6
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2022-08-23 22:45:00 +0700

    Implement getting Saving positions (#416)
    * Fixed invalid type of NextInterestPayDate field in StakingProductPosition struct
    
    * Updated module path to be be to install with go get
    
    * Fixed wrong mock test case
    
    * Implemented SavingFlexibleProductPositionsService to fetch the Saving flexible product positions
    
    * Renamed SavingProductPosition -> SavingFlexibleProductPosition
    
    * Implemented SavingFixedProjectPositionsService to fetch the Saving Fixed project positions
    
    * Added unit test for SavingFlexibleProductPositionsService
    
    * Added missing fields and wrong type in SavingFixedProjectPosition
    
    * Added unittest for SavingFixedProjectPosition
    
    * Renamed back to github.com/adshao

--- v2/savings_service_test.go
+++ v2/savings_service_test.go
@@ -246,3 +246,153 @@ func (s *savingsServiceTestSuite) assertSavingsFixedProductEqual(e, a *SavingsFi
 	r.Equal(e.Type, a.Type, "Type")
 	r.Equal(e.WithAreaLimitation, a.WithAreaLimitation, "WithAreaLimitation")
 }
+
+func (s *savingsServiceTestSuite) TestSavingFlexibleProductPositionsService() {
+	data := []byte(`[
+		{
+			"asset": "BUSD",
+			"productId": "BUSD001",
+			"productName": "BUSD",
+			"avgAnnualInterestRate": "0.09998802",
+			"annualInterestRate": "0.1",
+			"dailyInterestRate": "0.00017529",
+			"totalInterest": "12.95020362",
+			"totalAmount": "1234.56789",
+			"todayPurchasedAmount": "0",
+			"redeemingAmount": "0",
+			"freeAmount": "1234.56789",
+			"freezeAmount": "0",
+			"lockedAmount": "0",
+			"canRedeem": true
+	}
+]`)
+	s.mockDo(data, nil)
+	defer s.assertDo()
+	s.assertReq(func(r *request) {
+		e := newSignedRequest().setParams(params{})
+		s.assertRequestEqual(e, r)
+	})
+
+	flexibleProductList, err := s.client.NewSavingFlexibleProductPositionsService().
+		Asset("").
+		Do(newContext())
+	r := s.r()
+	r.NoError(err)
+
+	r.Len(flexibleProductList, 1)
+	s.assertSavingFlexibleProductPosition(&SavingFlexibleProductPosition{
+		Asset:                 "BUSD",
+		ProductId:             "BUSD001",
+		ProductName:           "BUSD",
+		AvgAnnualInterestRate: "0.09998802",
+		AnnualInterestRate:    "0.1",
+		DailyInterestRate:     "0.00017529",
+		TotalInterest:         "12.95020362",
+		TotalAmount:           "1234.56789",
+		TotalPurchasedAmount:  "0",
+		RedeemingAmount:       "0",
+		FreeAmount:            "1234.56789",
+		FreezeAmount:          "0",
+		LockedAmount:          "0",
+		CanRedeem:             true,
+	}, flexibleProductList[0])
+}
+
+func (s *savingsServiceTestSuite) assertSavingFlexibleProductPosition(e, a *SavingFlexibleProductPosition) {
+	r := s.r()
+	r.Equal(e.Asset, a.Asset, "Asset")
+	r.Equal(e.ProductId, a.ProductId, "ProductId")
+	r.Equal(e.ProductName, a.ProductName, "ProductName")
+	r.Equal(e.AvgAnnualInterestRate, a.AvgAnnualInterestRate, "AvgAnnualInterestRate")
+	r.Equal(e.AnnualInterestRate, a.AnnualInterestRate, "AnnualInterestRate")
+	r.Equal(e.DailyInterestRate, a.DailyInterestRate, "DailyInterestRate")
+	r.Equal(e.TotalInterest, a.TotalInterest, "TotalInterest")
+	r.Equal(e.TotalAmount, a.TotalAmount, "TotalAmount")
+	r.Equal(e.TotalPurchasedAmount, a.TotalPurchasedAmount, "TotalPurchasedAmount")
+	r.Equal(e.RedeemingAmount, a.RedeemingAmount, "RedeemingAmount")
+	r.Equal(e.FreeAmount, a.FreeAmount, "FreeAmount")
+	r.Equal(e.FreezeAmount, a.FreezeAmount, "FreezeAmount")
+	r.Equal(e.LockedAmount, a.LockedAmount, "LockedAmount")
+	r.Equal(e.CanRedeem, a.CanRedeem, "CanRedeem")
+}
+
+func (s *savingsServiceTestSuite) TestSavingFixedProjectPositionsService() {
+	data := []byte(`[
+		{
+			"asset": "USDT",
+			"canTransfer": true,
+			"createTimestamp": 1587010770000,
+			"duration": 14,
+			"startTime": 1587081600000,
+			"endTime": 1588291200000,
+			"purchaseTime": 1587010771000,
+			"interest": "0.19950000",
+			"interestRate": "0.05201250",
+			"lot": 1,
+			"positionId": 51724,
+			"principal": "100.00000000",
+			"projectId": "CUSDT14DAYSS001",
+			"projectName": "USDT",
+			"redeemDate": "2020-05-01",
+			"status": "HOLDING",
+			"type": "CUSTOMIZED_FIXED"
+	}
+]`)
+	s.mockDo(data, nil)
+	defer s.assertDo()
+	s.assertReq(func(r *request) {
+		e := newSignedRequest().setParams(params{
+			"status": "HOLDING",
+		})
+		s.assertRequestEqual(e, r)
+	})
+
+	positionsList, err := s.client.NewSavingFixedProjectPositionsService().
+		Asset("").
+		Status("HOLDING").
+		Do(newContext())
+	r := s.r()
+	r.NoError(err)
+
+	r.Len(positionsList, 1)
+	s.assertSavingFixedProjectPositionsService(&SavingFixedProjectPosition{
+		Asset:           "USDT",
+		CanTransfer:     true,
+		CreateTimestamp: 1587010770000,
+		Duration:        14,
+		StartTime:       1587081600000,
+		EndTime:         1588291200000,
+		PurchaseTime:    1587010771000,
+		Interest:        "0.19950000",
+		InterestRate:    "0.05201250",
+		Lot:             1,
+		PositionId:      51724,
+		Principal:       "100.00000000",
+		ProjectId:       "CUSDT14DAYSS001",
+		ProjectName:     "USDT",
+		RedeemDate:      "2020-05-01",
+		Status:          "HOLDING",
+		ProjectType:     "CUSTOMIZED_FIXED",
+	}, positionsList[0])
+}
+
+func (s *savingsServiceTestSuite) assertSavingFixedProjectPositionsService(e, a *SavingFixedProjectPosition) {
+	r := s.r()
+	r.Equal(e.Asset, a.Asset, "Asset")
+	r.Equal(e.CanTransfer, a.CanTransfer, "CanTransfer")
+	r.Equal(e.CreateTimestamp, a.CreateTimestamp, "CreateTimestamp")
+	r.Equal(e.Duration, a.Duration, "Duration")
+	r.Equal(e.StartTime, a.StartTime, "StartTime")
+	r.Equal(e.EndTime, a.EndTime, "EndTime")
+	r.Equal(e.PurchaseTime, a.PurchaseTime, "PurchaseTime")
+	r.Equal(e.Interest, a.Interest, "Interest")
+	r.Equal(e.InterestRate, a.InterestRate, "InterestRate")
+	r.Equal(e.Lot, a.Lot, "Lot")
+	r.Equal(e.PositionId, a.PositionId, "PositionId")
+	r.Equal(e.Principal, a.Principal, "Principal")
+	r.Equal(e.ProjectId, a.ProjectId, "ProjectId")
+	r.Equal(e.ProjectName, a.ProjectName, "ProjectName")
+	r.Equal(e.RedeemDate, a.RedeemDate, "RedeemDate")
+	r.Equal(e.Status, a.Status, "Status")
+	r.Equal(e.ProjectType, a.ProjectType, "ProjectType")
+}
