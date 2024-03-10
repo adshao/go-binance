f4b3272196b1828c95111bec0d4981fc76a3e518
Kenneth Tang <32535494+ChengYen-Tang@users.noreply.github.com>
2024-03-10 18:33:36 +0800

    [Bug]Funding Rate History response struct error (#544)
    * Update mark_price.go
    
    * Update unit test

--- v2/futures/mark_price.go
+++ v2/futures/mark_price.go
@@ -119,7 +119,7 @@ type FundingRate struct {
 	Symbol      string `json:"symbol"`
 	FundingRate string `json:"fundingRate"`
 	FundingTime int64  `json:"fundingTime"`
-	Time        int64  `json:"time"`
+	MarkPrice   string `json:"markPrice"`
 }
 
 // GetLeverageBracketService get funding rate
