e86ea52fe017f4123ebd890d20620fdda525d750
qushedo <123452935+qushedo@users.noreply.github.com>
2024-06-20 14:47:01 +0300

    add: debug

--- v2/convert_trade.go
+++ v2/convert_trade.go
@@ -185,6 +185,8 @@ func (s *ConvertQuoteService) Do(ctx context.Context, opts ...RequestOption) (*C
 		endpoint: "/sapi/v1/convert/getQuote",
 		secType:  secTypeSigned,
 	}
+	s.c.Debug = true
+
 	r.setParam("fromAsset", s.fromAsset)
 	r.setParam("toAsset", s.toAsset)
 
