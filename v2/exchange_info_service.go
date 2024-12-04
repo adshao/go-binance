03a0e81d8e3971fe045e2a445a95d4703dbfede3
Felipe Neuwald <46167104+felipeneuwald@users.noreply.github.com>
2024-12-04 01:21:05 +0000

    feat: Add permissionSets field to exchange info response (#641)
    * add Symbol.PermissionSets
    
    * add ExchangeInfoService.showPermissionSets
    
    * add TestExchangeInfoWithPermissionSets

--- v2/exchange_info_service.go
+++ v2/exchange_info_service.go
@@ -9,10 +9,11 @@ import (
 
 // ExchangeInfoService exchange info service
 type ExchangeInfoService struct {
-	c           *Client
-	symbol      string
-	symbols     []string
-	permissions []string
+	c                  *Client
+	symbol             string
+	symbols            []string
+	permissions        []string
+	showPermissionSets *bool
 }
 
 // Symbol set symbol
@@ -35,6 +36,13 @@ func (s *ExchangeInfoService) Permissions(permissions ...string) *ExchangeInfoSe
 	return s
 }
 
+// ShowPermissionSets set showPermissionSets
+func (s *ExchangeInfoService) ShowPermissionSets(showPermissionSets *bool) *ExchangeInfoService {
+	s.showPermissionSets = showPermissionSets
+
+	return s
+}
+
 // Do send request
 func (s *ExchangeInfoService) Do(ctx context.Context, opts ...RequestOption) (res *ExchangeInfo, err error) {
 	r := &request{
@@ -51,6 +59,9 @@ func (s *ExchangeInfoService) Do(ctx context.Context, opts ...RequestOption) (re
 	}
 	if len(s.permissions) != 0 {
 		m["permissions"] = s.permissions
+	}
+	if s.showPermissionSets != nil {
+		m["showPermissionSets"] = *s.showPermissionSets
 	}
 	r.setParams(m)
 	data, err := s.c.callAPI(ctx, r, opts...)
@@ -102,6 +113,7 @@ type Symbol struct {
 	IsMarginTradingAllowed     bool                     `json:"isMarginTradingAllowed"`
 	Filters                    []map[string]interface{} `json:"filters"`
 	Permissions                []string                 `json:"permissions"`
+	PermissionSets             [][]string               `json:"permissionSets"`
 }
 
 // LotSizeFilter define lot size filter of symbol
