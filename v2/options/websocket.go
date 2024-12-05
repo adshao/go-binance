4c9ea21a863a12605083f09c9ff19c1baac4447b
ddxyq <657023321@qq.com>
2024-12-05 22:13:09 +0800

    enable websocket compression (#645)

--- v2/options/websocket.go
+++ v2/options/websocket.go
@@ -40,7 +40,7 @@ var wsServe = func(cfg *WsConfig, handler WsHandler, errHandler ErrHandler) (don
 	Dialer := websocket.Dialer{
 		Proxy:             proxy,
 		HandshakeTimeout:  45 * time.Second,
-		EnableCompression: false,
+		EnableCompression: true,
 	}
 
 	c, _, err := Dialer.Dial(cfg.Endpoint, nil)
